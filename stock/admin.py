3f3bc6a757cd1a46d03695567f47adc92dcffa5c
jenish216 <77381917+jenish216@users.noreply.github.com>
2021-05-30 19:03:28 +0530

    First commit

--- stock/admin.py
+++ stock/admin.py
@@ -1,7 +1,12 @@
 
 from django.contrib import admin
 from .models import ContactInformation
+from .models import Form
 # Register your models here.
 @admin.register(ContactInformation)
 class UserAdmin(admin.ModelAdmin):
     list_display=("name","email","phoneNumber","city","msg")
+
+@admin.register(Form)
+class Form(admin.ModelAdmin):
+    list_display=("name","password","email","contact","address")
--- stockMarket/settings.py
+++ stockMarket/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stock.apps.StockConfig'
+    'stock.apps.StockConfig',
 ]
 
 MIDDLEWARE = [
